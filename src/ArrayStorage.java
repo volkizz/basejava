68399c7ee09aad3471b6457fbffd0b9e66a55933
Nick <mykolanagorskyi@gmail.com>
2019-11-11 15:01:17 -0600

    implementation of ArrayStorage methods clear, save, delete, get, getAll, size.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,52 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length - 1; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        List<Resume> resumeList = new ArrayList<>();
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) {
+                resumeList.add(storage[i]);
+            }
+        }
+        return resumeList.toArray(new Resume[0]);
     }
 
     int size() {
-        return 0;
+        return getAll().length;
     }
 }
