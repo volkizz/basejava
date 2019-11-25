93734e035f92444dfad0447d7e88dc5150308910
Nick <mykolanagorskyi@gmail.com>
2019-11-25 10:03:21 -0600

    ArrayStorage optimized

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -9,20 +9,15 @@ public class ArrayStorage {
     int size = 0;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                size++;
-                break;
-            }
-        }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
@@ -35,26 +30,25 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size() - 1; i++) {
+        for (int i = 0; i < size - 1; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 for (int j = i + 1; j < storage.length; j++) {
                     storage[j - 1] = storage[j];
                 }
             }
+            size--;
         }
-        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-
-        Resume[] all = new Resume[size()];
-        for (int i = 0; i < size(); i++){
-            all[i] = storage[i];
+        Resume[] resumes = new Resume[size()];
+        for (int i = 0; i < size; i++) {
+            resumes[i] = storage[i];
         }
-        return all;
+        return resumes;
     }
 
     int size() {
